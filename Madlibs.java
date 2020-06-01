622ca53883719fcbdf313b34c7e5a89c09c43cd8
wyattlaberge64 <42870908+wyattlaberge64@users.noreply.github.com>
2020-06-01 12:01:14 -0400

    Update Madlibs.java

--- Madlibs.java
+++ Madlibs.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* Mad libs - Java - Only version
@@ -13,15 +14,17 @@ public class Madlibs {
 		static int toReplace = 0;
 		static int replaceIndex = 0;
 		static String replacement = null;
-		static String replacements[];
-		static String newSentence = null;
+		static String newSentence;
+		static String prompt = null;
+		static int replacements[];
+		static String oldSentence;
 		
 		public static String[] getSentence()
 	    {
 	        Scanner in = new Scanner(System.in);
 	        System.out.print("Input a sentence for a madlib: ");
 	        String sentenceString = in.nextLine();
-	        in.close();
+	        //in.close();
 	        // switch statement with int data type
 	        try {
 	        	sentence = sentenceString.split(" ");
@@ -40,7 +43,40 @@ public class Madlibs {
 
 	    }
 
+		public static int getToReplace(String prompt)
+	    {
+	        Scanner input = new Scanner(System.in);
+	        System.out.print(prompt);
+	        int toReplace = 999;
+	        toReplace = input.nextInt();
+	        //input.close();
+	        // switch statement with int data type
+	        if(toReplace != (int)toReplace) {
+	        	System.out.print("Must be a numeral");
+	        	toReplace = getToReplace(prompt);
+	        }
+			return toReplace;
+	    }
 		
+		public static String getWord(int replaceIndex, String prompt)
+	    {
+	        Scanner inWord = new Scanner(System.in);
+	        System.out.print(prompt +sentence[replaceIndex]+"? ");
+	        String partOfSpeech = inWord.nextLine();
+	        //inWord.close();
+			return partOfSpeech;
+	    }
+		
+		public static void confirm(String prompt)
+	    {
+	        Scanner confirmPrompt = new Scanner(System.in);
+	        System.out.print(prompt);
+	        int toConfirm = confirmPrompt.nextInt();
+	        //input.close();
+	        // switch statement with int data type
+	        
+	    }
+		  
 		/* Function authorSetup
 	   * Author enters a sentence string. It is converted to an array. 
 	   * Author enters number of words to replace, then which ones, and replaces 
@@ -49,19 +85,23 @@ public class Madlibs {
 	   * @return none
 	   */  
 		
-//		public static void authorSetup(){
-//			sentenceString = prompt("Input a sentence for a madlib: ");
-//			sentence = sentenceString.split(" ");
-//			toReplace = prompt("How many words to replace? ");
-//			for(let i = 0; i < toReplace; i++){
-//				replaceIndex = prompt("Replace which word in sentence? ");
-//				replaceIndex--;
-//				replacements[i] = replaceIndex;
-//				let partOfSpeech = prompt("What part of speech is "+sentence[replaceIndex]+"? ");
-//				sentence[replaceIndex] = partOfSpeech;
-//			}
-//			System.out.print("Author, go get Player");
-//		}
+		public static void authorSetup(){
+			sentence = getSentence();
+			oldSentence = String.join(" ", sentence);
+			toReplace = getToReplace("How many words to replace? ");
+			replacements = new int[toReplace];
+			String partOfSpeech = null;
+			for(int i = 0; i < toReplace; i++){
+				replaceIndex = getToReplace("Replace which word in sentence? ");
+				replaceIndex--;
+				replacements[i] = replaceIndex;
+				System.out.println("i = " + i + " replacements[i] = " + replacements[i] + " replaceIndex = " + replaceIndex);
+				partOfSpeech = getWord(replaceIndex, "What part of speech is ");
+				sentence[replaceIndex] = partOfSpeech;
+			}
+			System.out.print("Author, go get Player");
+			System.out.println();
+		}
 		
 		/* Function playerPopulate
 	   * Ask player for replacement words.
@@ -69,12 +109,13 @@ public class Madlibs {
 	   * @return none
 	   */
 		
-//		public static void playerPopulate(){
-//			for(let i = 0; i < toReplace; i++){
-//				replacement = prompt("Enter a "+sentence[replacements[i]]);
-//				sentence[replacements[i]] = replacement;
-//			}
-//		}
+		public static void playerPopulate(){
+			for(int i = 0; i < toReplace; i++){
+				replaceIndex = replacements[i];
+				replacement = getWord(replaceIndex, "Enter a ");
+				sentence[replaceIndex] = replacement;
+			}
+		}
 		
 		/* Function displayResult
 	   * Creates a new sentence from the sentence array. 
@@ -84,13 +125,11 @@ public class Madlibs {
 	   * @return again (boolean)
 	   */
 		
-//		public static void displayResult(){
-//			newSentence = sentence.join(" ");
-//			let ready = confirm("Get Author, then press OK to see new sentence.");
-//			System.out.print(newSentence);
-//			let again = confirm("Play again?");
-//			return again;
-//		}
+		public static void displayResult(){
+			newSentence = String.join(" ", sentence);
+			confirm("Get Author, then enter a number to see new sentence.");
+			System.out.print("The old sentence was "+ oldSentence + ", and the new sentence is " + newSentence + ".");
+		}
 		
 		/* Function Main
 		 * Manages game play: initializes variables and calls authorSetup and playerPopulate.  
@@ -99,17 +138,17 @@ public class Madlibs {
 		 * @param none
 		 * @return none
 		 */
-		public static void main(){
+		public static void main(String args[]){
 			// initialize variables (in one line!)
 			
 			// function calls
-//			authorSetup();
-//			playerPopulate();
+			authorSetup();
+			playerPopulate();
+			displayResult();
 //			if(displayResult() = true){
 //			main();
 //			}
 //			System.out.print("Thanks for playing");
-			System.out.print(getSentence());
 		}
 	// End Function Main
 }
